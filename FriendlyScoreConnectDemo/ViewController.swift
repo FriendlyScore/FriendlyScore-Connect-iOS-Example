c7a80e1b3c35f2607a779e18c62cf546d58854f3
deepak <d.bhatia@friendlyscore.com>
2021-05-20 11:34:05 +0200

    variable

--- FriendlyScoreConnectDemo/ViewController.swift
+++ FriendlyScoreConnectDemo/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
 
     @IBAction func launchTapHandler(_ sender: Any) {
         //You need to Sign-up for the free API keys through our Developer Console.
-        let clientId = ClientId(stringLiteral: "YOUR_CLIENT_ID")
+        let clientId = ClientId(stringLiteral: "[YOUR_CLIENT_ID]")
         let credentials = Credentials(clientId: clientId, userReference: "user_reference", environment: .production)
         
         FriendlyScore.eventsHandler = { event in
